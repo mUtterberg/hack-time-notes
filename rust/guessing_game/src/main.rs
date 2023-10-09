262f3d37b157d52f234c9a69384e291723d3c11d
mutterberg <mutterberg@invoca.com>
2023-10-09 12:40:14 -0400

    Allow multiple guesses & exit on winning guess

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -9,25 +9,32 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    // Could also implement std::io::stdin without the use statement
-    io::stdin()
+    loop {
+        
+        
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
+        
+        // Could also implement std::io::stdin without the use statement
+        io::stdin()
         .read_line(&mut guess)
         // The right way to suppress the warning is to actually write error-handling code,
         // but in our case we just want to crash this program when a problem occurs,
         // so we can use expect. 
         .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+    
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
+        }
     }
 }
