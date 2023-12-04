e80d18406cd89548205df92213fcad25f3d377b3
mutterberg <mutterberg@invoca.com>
2023-12-04 11:41:20 -0500

    Move examples to new code files

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,28 +1,3 @@
 fn main() {
-    mutable_reference();
-    safe_run();
-}
-
-fn safe_run() {
-    let x = true;
-    read(x);
-}
-
-// fn unsafe_run() {
-//     read(x);
-//     let x = true;
-// }
-
-fn read(y: bool) {
-    if y {
-        println!("y is true!");
-    }
-}
-
-fn mutable_reference() {
-    let mut v: Vec<i32> = vec![1, 2, 3];
-    let num: &mut i32 = &mut v[2];
-    *num += 1;
-    println!("Third element is {}", *num);
-    println!("Vector is now {:?}", v);
+    println!("Hello, world!");
 }
---
+++ rust/ownership/src/safe_vs_unsafe_run.rs
@@ -0,0 +1,20 @@
+fn main() {
+    safe_run();
+    unsafe_run();
+}
+
+fn safe_run() {
+    let x = true;
+    read(x);
+}
+
+fn unsafe_run() {
+    read(x);
+    let x = true;
+}
+
+fn read(y: bool) {
+    if y {
+        println!("y is true!");
+    }
+}
